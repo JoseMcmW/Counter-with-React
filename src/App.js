cb69f91066896f5423aaeeff73fac5b9058758a9
Jose Centeno <josemanuelcm25@gmail.com>
2022-05-19 17:39:10 -0300

    counter finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  let countStart = 0;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='title'>
+        <h1 className='title-2'>Counter</h1>
+      </div>
+      <div className='counterNumber'>{count}</div>
+      <div className='buttons'>
+        <button className='button' onClick={() => setCount(count + 1)}>Suma</button>
+        <button className='button' onClick={() => setCount(count - 1)}>Resta</button>
+        <button className='button' onClick={() => setCount(countStart)}>Reset</button>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
